30e1a5f05eb58fc21fa12120d1853b660d435f38
khartmann <kevin.hartmann@oicr.on.ca>
2017-10-30 14:28:27 -0400

    Bugfix: Name should be icgc, not icgc-client, to match the module name

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
 
 setup(
-    name='icgc-client',
+    name='icgc',
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
