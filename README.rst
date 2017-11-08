e2545049742d74dda02d5bb0e32df44b978a0ec8
khartmann <kevin.hartmann@oicr.on.ca>
2017-11-08 14:13:44 -0500

    Enhancement: Simplified API. Enhancement: Updated ReadME to match new API Enhancement: Added support for filter downloads (needs support for PQL queries on the server end) Bugfix: Changed copyright headers to use Python, not Java, style comments

--- README.rst
+++ README.rst
@@ -12,10 +12,48 @@ Here's an example that shows you how easy it is to get started!
 ::
 
     import icgc
-    client = icgc.Client()
-    # get information about donors as a JSON object
-    donor_info=client.query(request_type='donors',pql='select(*)')    	
-    print(donor_info)
+    local_server="http://localhost:8080/api/v1/"
+    for request_type in icgc.request_types():
+        response = icgc.query(request_type=request_type, pql='select(*)',base_url=local_server)
+        print(request_type, "===\n\n", response)
+
+Here's an example that demonstrates downloading some donor data
+::
+
+    import icgc 
+    
+    KB=1024
+    MB=1024*KB
+    
+    filters='{"donor":{"primarySite":{"is":["Brain"]}}}'
+    
+    # See what different items are available for our current choice of filter 
+    # and how big they are
+    
+    sizes = icgc.download_size(filters)
+    print("Sizes are: {}".format(sizes))
+    
+    # Let's choose to download as many different bits of information as we can fit 
+    # in a reasonably small download, say 10 MB in size, and just download those
+    # ones.
+    
+    max_size=10 * MB 
+    current_size=0
+    
+    # We'll just keep adding items to our list as long as they don't put us over our
+    # download total.
+    includes=[]
+    for k in sizes:
+        item_size=sizes[k]
+        if current_size + item_size < max_size: 
+           includes.append(k)
+           current_size += item_size
+    
+    print("Including items {}".format(includes))
+    print("Approximate download size={:.2f} MB".format(current_size / MB))
+    
+    # Download the information, and save the results in the file "test1.tar"
+    icgc.download(filters,includes,"test1")
 
 Installation
 ------------
--- icgc/__init__.py
+++ icgc/__init__.py
@@ -1,20 +1,20 @@
-/*
- * Copyright (c) 2017 The Ontario Institute for Cancer Research. All rights reserved.                             
- *                                                                                                               
- * This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
- * You should have received a copy of the GNU General Public License along with                                  
- * this program. If not, see <http://www.gnu.org/licenses/>.                                                     
- *                                                                                                               
- * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY                           
- * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES                          
- * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT                           
- * SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,                                
- * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED                          
- * TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;                               
- * OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER                              
- * IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN                         
- * ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- */
+#
+# Copyright (c) 2017 The Ontario Institute for Cancer Research. All rights reserved.
+#
+# This program and the accompanying materials are made available under the terms of the GNU Public License v3.0.
+# You should have received a copy of the GNU General Public License along with
+# this program. If not, see <http://www.gnu.org/licenses/>.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY
+# EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+# OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT
+# SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT,
+# INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED
+# TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+# OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN
+# ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
 """
 The ICGC module implements a simple Python REST client that can be used
 to access our web portal
@@ -22,6 +22,7 @@ to access our web portal
 
 import requests
 
+BASE_URL="http://localhost:8080/api/v1/"
 
 def fmt(format_string, *a, **kw):
     """
@@ -34,75 +35,98 @@ def fmt(format_string, *a, **kw):
     """
     return format_string.format(*a, **kw)
 
+def report(name):
+    n=1
+    i=n
+    total_bytes=0
+    threshold=1024*1024;
+    total_bytes=0
+    while True:
+          data_bytes= yield(total_bytes)
+          total_bytes += data_bytes
+          #print("Downloaded {} bytes".format(total_bytes))
+          if total_bytes > i * threshold: 
+             print("Downloaded {} MB to {}.tar".format(i,name))
+             i+=n
 
-class Client(object):
+def download(filters,include,name,fmt='TSV',base_url=BASE_URL, reporter=None):
+    if reporter is None:
+       reporter=report
+    includes=[] 
+    for k in include:
+        txt ="{" + '"key":"{}", "value": "{}"'.format(k,fmt) +"}"
+        includes.append(txt)
+    url1=base_url + "download/submit?filters={}&info=[{}]".format(filters,",".join(includes))
+    print("Sending request {}".format(url1))
+    r=requests.get(url1)
+    url2 =base_url + "download/{}".format(r.json()['downloadId'])
+    r2=requests.request('GET',url2,headers={'Accept':'application/x-tar'},verify=False,stream=True)
+    x = reporter(name)
+    x.send(None)
+
+    with open(name+".tar","wb") as f:
+         for data in r2.iter_content(chunk_size=None):
+             size=x.send(len(data)) 
+             f.write(data)
+
+    x.close()
+
+def download_size(filters,base_url=BASE_URL):
+    url= base_url + "download/size?filters={}".format(filters)
+
+    sizes=get_data(url)['fileSize'] 
+    d={}
+    for entry in sizes:
+        key=entry['label']
+        value=entry['sizes']
+        d[key]=value
+    return d
+
+def request_types():
     """
-    The Client class is a simple python REST client for accessing the data
-    available through the ICGC web portal.
+    Return a list of valid request types
+    :return: A list of strings naming valid request types
+    """
+    return ["donors", "genes", "mutations", "occurrences"]
+
+def formats():
+    """
+    Return a list of valid output formats
+    :return: A list of strings naming valid output formats
+    """
+    return ["json"]
+
+def query(request_type, pql, output_format='json',base_url=BASE_URL):
+    """
+    Validate the query request, and return the results from the portal server
+    :param request_type: Type of request: must be a string from "request_types"
+    :param pql: Must be a valid PQL request for the given request_type
+    :param output_format: Must be a string containing one of the valid output formats
+    :return: The results from running the PQL query on the portal server 
     """
 
-    def __init__(self, url="http://dcc.icgc.org", authentication=None):
-        """
-        Create an icgc client object, capable of connecting to our servers
-        and fetching data
-        :param url: Should we set a default url here?
-        :param authentication: The authentication token required to run the
-        query (currently not used yet)
-        """
-        self.url = url
-        self.authentication = authentication
+    if request_type not in request_types():
+       raise TypeError(fmt("Invalid Request Type {}, must be one of {}",
+           request_type, request_types()))
+    if output_format not in formats():
+        raise TypeError(fmt("Invalid format {}, must be one of {}",
+                                output_format, formats()))
 
-    @classmethod
-    def request_types(cls):
-        """
-        Return a list of valid request types
-        :return: A list of strings naming valid request types
-        """
-        return ["donors", "genes", "mutations", "occurrences"]
+    url = base_url + fmt("{}/pql?query={}", request_type, pql)
 
-    @classmethod
-    def formats(cls):
-        """
-        Return a list of valid output formats
-        :return: A list of strings naming valid output formats
-        """
-        return ["json"]
+    return get_data(url)
 
-    def query(self, request_type, pql, output_format='json'):
-        """
-        Validate the query request, and return the results from the portal
-        server
-        :param request_type: Type of request: must be a string from
-        "request_types"
-        :param pql: Must be a valid PQL request for the given request_type
-        :param output_format: Must be a string containing one of the valid
-        output formats
-        :return: The results from running the PQL query on the portal server
-        in the specified output format
-        """
-        if request_type not in self.request_types():
-            raise TypeError(fmt("Invalid Request Type {}, must be one of {}",
-                                request_type, self.request_types()))
-        if output_format not in self.formats():
-            raise TypeError(fmt("Invalid format {}, must be one of {}",
-                                output_format, self.formats()))
+def get_data(url):
+    """
+    Helper method used to issue a request to the portal server's rest
+    api, and get a result back
+    :param url: The url to contact the server API with
+    :return: The results from the server as a 'requests' response object;
+        raises an IOError if we can't connect.
+    """
 
-        url = self.url + fmt("/{}/pql?query={}", request_type, pql)
-
-        return self.get_data(url)
-
-    @staticmethod
-    def get_data(url):
-        """
-        Helper method used to issue a request to the portal server's rest
-        api, and get a result back
-        :param url: The url to contact the server API with
-        :return: The results from the server as a 'requests' response object;
-            raises an IOError if we can't connect.
-        """
-
-        # print(f"Fetching url {url}")
-        resp = requests.get(url)
-        if resp.status_code != 200:
-            raise IOError(fmt('GET {} {}', url, resp.status_code))
-        return resp.json()
+    # print(f"Fetching url {url}")
+    resp = requests.get(url)
+    if resp.status_code != 200:
+        raise IOError(fmt('GET {} {}', url, resp.status_code))
+    return resp.json()
